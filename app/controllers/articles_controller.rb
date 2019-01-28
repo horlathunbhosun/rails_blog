f070bf279e2fd3a7e1f238493a8867ff1175ed89
horlathunbhosun <horlathunbhosun@gmail.com>
2019-01-28 14:52:47 +0100

    edit articles , show articles done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 	def index
-		
+		@articles = Article.all
 	end
 
 	def new
@@ -21,8 +21,18 @@ class ArticlesController < ApplicationController
 		
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def update
-		
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Your article as been updated successfully"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
