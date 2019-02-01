f81c19418207bb955eb78e98a2da2525236858c9
horlathunbhosun <horlathunbhosun@gmail.com>
2019-02-01 19:49:31 +0100

    add association between user and articles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
 class Article < ApplicationRecord
-		validates :title, presence: true, 
-				  length: {minimum: 3 , maximum:50}
-		validates  :description, presence: true,
-				   length: {minimum: 3, maximum:30000}
+  belongs_to :user
+  validates :title, presence: true,
+                    length: {minimum: 3, maximum: 50}
+  validates :description, presence: true,
+                          length: {minimum: 3, maximum: 30000}
+  validates :user_id, presence: true
 
-		#Mounts paperclip image
-		has_attached_file :image
-		validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  #Mounts paperclip image
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
