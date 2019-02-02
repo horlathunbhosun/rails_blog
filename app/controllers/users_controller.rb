592643ccba5ce91eb6c7a7386e93322d3caff1e0
horlathunbhosun <horlathunbhosun@gmail.com>
2019-02-02 21:56:43 +0100

    added edit update logic and form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,21 +9,27 @@ class UsersController < ApplicationController
 
     end
    def create
-       @user = User.new(user_params)
-       if @user.save
-         flash[:success] = "Welcome to Asf Blog #{@user.username}"
-         redirect_to articles_path
-       else
-         render 'new'
-       end
+     @user = User.new(user_params)
+     if @user.save
+       flash[:success] = "Welcome to Asf Blog #{@user.username}"
+       redirect_to articles_path
+     else
+       render 'new'
+     end
    end
 
     def edit
-
+      @user = User.find(params[:id])
     end
 
     def update
-
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Your profile has been edited successfully"
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
     end
 
     def destroy
