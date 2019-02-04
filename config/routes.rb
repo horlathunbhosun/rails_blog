13f1f032d1ec2e3498e30ce0e4ced356197a67a0
horlathunbhosun <horlathunbhosun@gmail.com>
2019-02-04 16:05:48 +0100

    added session controller,views and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
   get 'signup' => 'users#new'
   resources :users, except: [:new]
+  get 'login' =>  'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'session#destroy'
 end
