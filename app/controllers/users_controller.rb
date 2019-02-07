441300896426e633f6e3e15e4e6f16e12e80779f
horlathunbhosun <horlathunbhosun@gmail.com>
2019-02-07 10:36:26 +0100

    user signup wrap up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :show, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only:[:destroy]
   def new
     @user = User.new
   end
@@ -37,7 +38,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-
+  @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = " User and all article created by the user as been deleted"
+    redirect_to users_path
   end
 
   private
@@ -50,8 +54,16 @@ class UsersController < ApplicationController
 
   end
   def require_same_user
-    if  current_user != @user
-      flash[:danger] = "You can only edit your profile"
+    if current_user != @user and !current_user.admin?
+        flash[:danger] = "You can only edit your profile"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190202190901) do
+ActiveRecord::Schema.define(version: 20190207073241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20190202190901) do
     t.string "password_digest"
     t.string "firstname"
     t.string "lastname"
+    t.boolean "admin", default: false
   end
 
 end
